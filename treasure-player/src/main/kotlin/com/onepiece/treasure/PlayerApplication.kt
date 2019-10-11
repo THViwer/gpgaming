6ad825732dd432e990161d6d8423c85880a71551
cabbage <cabbage.cgh@gmail.com>
2019-10-12 01:02:56 +0800

    init

---
+++ treasure-player/src/main/kotlin/com/onepiece/treasure/PlayerApplication.kt
@@ -0,0 +1,12 @@
+package com.onepiece.treasure
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class PlayerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlayerApplication>()
+}
