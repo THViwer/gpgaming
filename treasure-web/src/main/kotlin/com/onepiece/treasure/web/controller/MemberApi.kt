8ff1e56a23e4e99fcfe2c821e60af55121442117
cabbage <cabbage.cgh@gmail.com>
2019-10-22 18:46:21 +0800

    init

---
+++ treasure-web/src/main/kotlin/com/onepiece/treasure/web/controller/MemberApi.kt
@@ -0,0 +1,37 @@
+package com.onepiece.treasure.web.controller
+
+import com.onepiece.treasure.account.model.enums.Status
+import com.onepiece.treasure.web.controller.value.BalanceDetail
+import com.onepiece.treasure.web.controller.value.MemberPage
+import com.onepiece.treasure.web.controller.value.MemberUo
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@Api(tags = ["member"], description = " ")
+interface MemberApi {
+
+    @ApiOperation(tags = ["member"], value = "query")
+    fun query(
+            @RequestParam(value = "id") id: Int,
+            @RequestParam(value = "username", required = false) username: String?,
+            @RequestParam(value = "levelId", required = false) levelId: Int?,
+            @RequestParam(value = "status", required = false) status: Status?,
+            @RequestParam(defaultValue = "0") current: Int,
+            @RequestParam(defaultValue = "10") size: Int
+    ): MemberPage
+
+
+    @ApiOperation(tags = ["member"], value = "update")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    fun change(
+            @RequestBody memberUo: MemberUo
+    )
+
+    @ApiOperation(tags = ["member"], value = "balance detail")
+    fun balance(@PathVariable("memberId") memberId: Int): BalanceDetail
+}
---
+++ treasure-web/src/main/kotlin/com/onepiece/treasure/web/controller/MemberApiController.kt
@@ -0,0 +1,38 @@
+package com.onepiece.treasure.web.controller
+
+import com.onepiece.treasure.account.model.enums.Status
+import com.onepiece.treasure.web.controller.value.*
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/member")
+class MemberApiController : MemberApi {
+
+    @GetMapping
+    override fun query(
+            @RequestParam(value = "id") id: Int,
+            @RequestParam(value = "username", required = false) username: String?,
+            @RequestParam(value = "levelId", required = false) levelId: Int?,
+            @RequestParam(value = "status", required = false) status: Status?,
+            @RequestParam(defaultValue = "0") current: Int,
+            @RequestParam(defaultValue = "10") size: Int
+    ): MemberPage {
+
+        val query = MemberQuery(id = id, username = username, levelId = levelId, status = status,
+                current = current, size = size)
+
+        return MemberValueFactory.generatorMemberPage()
+    }
+
+    @PutMapping
+    override fun change(
+            @RequestBody memberUo: MemberUo
+    ) {
+
+    }
+
+    @GetMapping("/balance/{memberId}")
+    override fun balance(@PathVariable("memberId") memberId: Int): BalanceDetail {
+        return BalanceValueFactory.generatorBalanceDetail()
+    }
+}
