f20295ff18ed7756dc2c68d32a4991b0da41f281
cabbage <cabbage.cgh@gmail.com>
2019-11-07 23:20:44 +0800

    init

--- games/src/main/kotlin/com/onepiece/treasure/games/GameConstant.kt
+++ games/src/main/kotlin/com/onepiece/treasure/games/GameConstant.kt
@@ -3,14 +3,18 @@ package com.onepiece.treasure.games
 object GameConstant  {
 
     // joker
-    const val JOKER_URL = "http://api688.net:81"
+//    const val JOKER_URL = "http://api688.net:81"
+    const val JOKER_URL = "http://94.237.64.70:81"
     const val JOKER_GAME_URL = "http://94.237.64.70/iframe.html"
 
     // cta666
-    const val CTA666_URL = "http://api.ctapi888.com"
+//    const val CTA666_URL = "http://api.ctapi888.com"
+    const val CTA666_URL = "http://94.237.64.70:82"
 
     // 918kiss
-    const val KISS918_API_URL = "http://api.918kiss.com:9991"
-    const val KISS918_API_ORDER_URL = "http://api.918kiss.com:9919"
+//    const val KISS918_API_URL = "http://api.918kiss.com:9991"
+    const val KISS918_API_URL = "http://94.237.64.70:83"
+//    const val KISS918_API_ORDER_URL = "http://api.918kiss.com:9919"
+    const val KISS918_API_ORDER_URL = "http://94.237.64.70:84"
 
 }
