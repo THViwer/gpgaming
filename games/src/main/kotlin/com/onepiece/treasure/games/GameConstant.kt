f419fc628c2f6d1456b4a96a85dd92fe5e907445
cabbage <cabbage.cgh@gmail.com>
2019-11-27 03:27:48 +0800

    完成TTG

--- games/src/main/kotlin/com/onepiece/treasure/games/GameConstant.kt
+++ games/src/main/kotlin/com/onepiece/treasure/games/GameConstant.kt
@@ -20,62 +20,80 @@ class GameConstant(
         }
 
         return when (platform) {
-            Platform.GGFishing -> "https://optest.365gaming.cc:10029"
-            Platform.DreamGaming -> "http://api.dg99web.com"
-            Platform.Lbc -> "http://tsa.gpgaming88.com"
-            Platform.Pragmatic -> " https://api.prerelease-env.biz"
-            Platform.SpadeGaming -> "http://api-egame-staging.sgplay.net"
-            Platform.SexyGaming -> ""
-            Platform.AllBet -> "https://api3.apidemo.net:8443"
-            Platform.Bcs -> "https://transferapi.ballcrown.com"
-            Platform.Fgg -> "https://d-tapi.fgg365.com"
+
+            // slot
+            Platform.Joker -> "http://api688.net:81"
+            Platform.Kiss918 -> "http://api.918kiss.com:9991"
             Platform.Pussy888 -> "http://api.pussy888.com"
             Platform.Mega -> "http://mgt3.36ozhushou.com/mega-cloud"
-            Platform.GoldDeluxe -> "http://api.coldsstaging.stack4kids.com"
-            Platform.Evolution -> "http://staging.evolution.asia-live.com"
-//            Platform.Sbo -> "http://sboapi.gsoft168.com"
-            Platform.Kiss918 -> "http://api.918kiss.com:9991"
-            Platform.CT -> "http://api.ctapi888.com"
-            Platform.Joker -> "http://api688.net:81"
+            Platform.Pragmatic -> " https://api.prerelease-env.biz"
+            Platform.SpadeGaming -> "http://api-egame-staging.sgplay.net"
             Platform.TTG -> "https://ams-api.stg.ttms.co:8443"
+
+            // live
+            Platform.CT -> "http://api.ctapi888.com"
+            Platform.DreamGaming -> "http://api.dg99web.com"
+            Platform.Evolution -> "http://staging.evolution.asia-live.com"
+            Platform.GoldDeluxe -> "http://api.coldsstaging.stack4kids.com"
+            Platform.SexyGaming -> "https://testapi.onlinegames22.com"
+            Platform.Fgg -> "https://d-tapi.fgg365.com"
+            Platform.AllBet -> "https://api3.apidemo.net:8443"
+
+            // sport
+            Platform.Lbc -> "http://tsa.gpgaming88.com"
+            Platform.Bcs -> "https://transferapi.ballcrown.com"
+//            Platform.Sbo -> "http://sboapi.gsoft168.com"
+
+            // fishing
+            Platform.GGFishing -> "https://optest.365gaming.cc:10029"
 
             else -> error(OnePieceExceptionCode.DATA_FAIL)
         }
     }
 
     private fun getDevDomain(platform: Platform): String {
+
+        val domain = "http://94.237.64.70"
         return  when (platform) {
-            Platform.GGFishing -> "${proxy}/gg"
-            Platform.DreamGaming -> "${proxy}/dreamGaming"
             Platform.Lbc -> "http://45.124.64.29:88"
-            Platform.Pragmatic -> "https://api.prerelease-env.biz"
-            Platform.SpadeGaming -> "${proxy}/spadeGaming"
-            Platform.SexyGaming -> ""
-            Platform.AllBet -> "${proxy}/allbet"
-            Platform.Bcs -> "${proxy}/bcs"
-            Platform.Fgg -> "${proxy}/fgg/Game"
-            Platform.Pussy888 -> "${proxy}/pussy"
-            Platform.Mega -> "${proxy}/mega"
-            Platform.GoldDeluxe -> "$proxy/goldDeluxe"
-            Platform.Evolution -> "${proxy}/evolution"
-//            Platform.Sbo -> "${proxy}/sbo"
-            Platform.Kiss918 -> "${proxy}/kiss918"
-            Platform.CT -> "${proxy}/ct"
-            Platform.Joker -> "${proxy}/joker"
-//            Platform.TTG -> "${proxy}/ttg"
-            Platform.TTG -> "http://94.237.64.70:106"
+//            Platform.Pragmatic -> "https://api.prerelease-env.biz"
+
+            // slot
+            Platform.Joker -> "$domain:1001"
+            Platform.Kiss918 -> "$domain:1002"
+            Platform.Pussy888 -> "$domain:1003"
+            Platform.Mega -> "$domain:1004"
+            Platform.Pragmatic -> "$domain:1005"
+            Platform.SpadeGaming -> "$domain:1006"
+            Platform.TTG -> "$domain:1007"
+
+            // live
+            Platform.CT -> "$domain:2001"
+            Platform.DreamGaming -> "$domain:2002"
+            Platform.Evolution -> "$domain:2003"
+            Platform.GoldDeluxe -> "$domain:2004"
+            Platform.SexyGaming -> "$domain:2005"
+            Platform.Fgg -> "$domain:2006"
+            Platform.AllBet -> "$domain:2007"
+
+            // sport
+            Platform.Bcs -> "$domain:3001"
+
+            // fishing
+            Platform.GGFishing -> "$domain:4001"
 
             else -> error(OnePieceExceptionCode.DATA_FAIL)
         }
     }
 
     fun getOrderApiUrl(platform: Platform): String {
+        val domain = "http://94.237.64.70"
         return when {
-            platform == Platform.Pussy888 && activeConfig.profile == "dev" -> "$proxy/pussyOrder"
+            platform == Platform.Pussy888 && activeConfig.profile == "dev" -> "$domain:7001"
             platform == Platform.Pussy888 -> "http://api2.pussy888.com"
-            platform == Platform.Kiss918 && activeConfig.profile == "dev" -> "$proxy/kiss918Order"
+            platform == Platform.Kiss918 && activeConfig.profile == "dev" -> "$domain:7002"
             platform == Platform.Kiss918 -> "http://api.918kiss.com:9919"
-            platform == Platform.TTG && activeConfig.profile == "dev" -> "http://94.237.64.70:107"
+            platform == Platform.TTG && activeConfig.profile == "dev" -> "$domain:7003"
             platform == Platform.TTG -> "https://ams-df.stg.ttms.co:7443"
             else -> error(OnePieceExceptionCode.DATA_FAIL)
         }
