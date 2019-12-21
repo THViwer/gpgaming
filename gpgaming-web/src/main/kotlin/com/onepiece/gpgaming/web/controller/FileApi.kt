bb67c65dfe53f47cafedcc60e2de33545568e7ae
cabbage <cabbage.cgh@gmail.com>
2019-12-21 21:36:14 +0800

    init

--- gpgaming-web/src/main/kotlin/com/onepiece/gpgaming/web/controller/FileApi.kt
+++ gpgaming-web/src/main/kotlin/com/onepiece/gpgaming/web/controller/FileApi.kt
@@ -1,6 +1,5 @@
 package com.onepiece.gpgaming.web.controller
 
-import com.onepiece.gpgaming.beans.enums.FileCategory
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.RequestParam
@@ -11,7 +10,7 @@ interface FileApi {
 
     @ApiOperation(tags = ["index"], value = "上传图片")
     fun uploadProof(
-            @RequestParam("category") category: FileCategory,
+            @RequestParam("category") category: String,
             @RequestParam("file") file: MultipartFile): Map<String, String>
 
 
--- gpgaming-web/src/main/kotlin/com/onepiece/gpgaming/web/controller/FileApiController.kt
+++ gpgaming-web/src/main/kotlin/com/onepiece/gpgaming/web/controller/FileApiController.kt
@@ -1,7 +1,6 @@
 package com.onepiece.gpgaming.web.controller
 
 import com.onepiece.gpgaming.beans.SystemConstant
-import com.onepiece.gpgaming.beans.enums.FileCategory
 import com.onepiece.gpgaming.beans.exceptions.OnePieceExceptionCode
 import com.onepiece.gpgaming.games.ActiveConfig
 import com.onepiece.gpgaming.utils.AwsS3Util
@@ -20,16 +19,23 @@ class FileApiController(
 
     @PostMapping("/upload")
     override fun uploadProof(
-            @RequestParam("category") category: FileCategory,
+            @RequestParam("category") category: String,
             @RequestParam("file") file: MultipartFile
     ): Map<String, String> {
         val clientId = getClientId()
 
         val path = when (category) {
-            FileCategory.Banner -> "banner"
-            FileCategory.IndexLive -> "live"
-            FileCategory.IndexSport -> "sport"
-            FileCategory.IndexVideo -> "video"
+            "promotion" -> "promotion"
+            "banner" -> "banner"
+            "Banner" -> "banner"
+            "contact" -> "contact"
+            "IndexLive" -> "live"
+            "IndexSport" -> "sport"
+            "IndexVideo" -> "video"
+//            FileCategory.Banner -> "banner"
+//            FileCategory.IndexLive -> "live"
+//            FileCategory.IndexSport -> "sport"
+//            FileCategory.IndexVideo -> "video"
             else -> error(OnePieceExceptionCode.DATA_FAIL)
         }.let {
             SystemConstant.getClientResourcePath(clientId = clientId, profile = activeConfig.profile, defaultPath = it)
