b95eef16e55b4bea888748e2aff6b17f4b94729d
cabbage <cabbage.cgh@gmail.com>
2019-12-29 15:10:19 +0800

    init

--- core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/enums/PromotionCategory.kt
+++ core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/enums/PromotionCategory.kt
@@ -9,4 +9,12 @@ enum class PromotionCategory(
 
     Special("special promotion", "特别优惠"),
 
+    Slot("slot", "老虎机"),
+
+    Live("live casino", "真人视频"),
+
+    Sport("sport", "体育"),
+
+    Fishing("fishing", "捕鱼")
+
 }
