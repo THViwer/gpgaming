3316aff25515054eeb71348b2390a7c7fc4be590
cabbage <cabbage.cgh@gmail.com>
2020-03-12 16:36:01 +0800

    添加游戏列表

--- gpgaming-task/src/main/kotlin/com/onepiece/gpgaming/task/PullBetTask.kt
+++ gpgaming-task/src/main/kotlin/com/onepiece/gpgaming/task/PullBetTask.kt
@@ -49,8 +49,8 @@ class PullBetTask(
         //TODO 暂时过滤其它厅主的
 //        val binds = platformBindService.all().filter { it.platform == Platform.AsiaGamingSlot || it.platform == Platform.AsiaGamingLive }.filter { it.clientId == 1 } // && it.platform == Platform.MicroGaming
         val binds = platformBindService.all()
-                .filter { it.clientId == 1  }
-                .filter { it.platform == Platform.Pragmatic }
+//                .filter { it.clientId == 1  }
+//                .filter { it.platform == Platform.Pragmatic }
 
         binds.filter { it.platform != Platform.PlaytechLive }.parallelStream().forEach  { bind ->
             this.executePlatform(bind)
