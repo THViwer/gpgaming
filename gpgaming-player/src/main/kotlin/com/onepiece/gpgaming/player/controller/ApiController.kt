1974d4248391b822232b72518e8b70279e3dfa02
cabbage <cabbage.cgh@gmail.com>
2020-03-16 19:41:17 +0800

    添加游戏列表

--- gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/controller/ApiController.kt
+++ gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/controller/ApiController.kt
@@ -327,7 +327,7 @@ open class ApiController(
                 detail.username to detail.password
 
                 val clientToken = platformBindService.find(member.clientId, platform).clientToken as PlaytechClientToken
-                StartGameResp(path = "-", username = detail.username, password = detail.password, params = hashMapOf("serverName" to clientToken.serverName))
+                StartGameResp(path = "-", username = detail.username, password = detail.password, params = hashMapOf("serverName" to clientToken.serverName.toLowerCase()))
             }
             else -> {
                 val gameUrl = gameApi.start(clientId = member.clientId, platformUsername = platformMember.platformUsername, platform = platform,
