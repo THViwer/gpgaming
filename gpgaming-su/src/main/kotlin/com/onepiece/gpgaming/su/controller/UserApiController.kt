72755fc2c73e2b01f5ce6a14a7f5762bc9194ddc
cabbage <cabbage.cgh@gmail.com>
2020-03-16 11:30:50 +0800

    添加游戏列表

--- gpgaming-su/src/main/kotlin/com/onepiece/gpgaming/su/controller/UserApiController.kt
+++ gpgaming-su/src/main/kotlin/com/onepiece/gpgaming/su/controller/UserApiController.kt
@@ -13,18 +13,8 @@ import java.util.*
 class UserApiController : UserApi {
 
     @PostMapping("/login")
-    override fun login(@RequestBody loginReq: LoginValue.LoginReq): LoginValue.LoginResp {
-
-        check(loginReq.username == "su")
-        check(loginReq.password == "GPGaming")
-
-        return LoginValue.LoginResp(id = 1, username = "su", token = UUID.randomUUID().toString())
-
-    }
-
-    @PostMapping("/login")
     override fun login(@RequestBody loginReq: UserValue.LoginReq): UserValue.LoginRes {
-        check(loginReq.username == "su" && loginReq.password == "123456") { "用户名或密码错误" }
+        check(loginReq.username == "su" && loginReq.password == "GPGaming") { "用户名或密码错误" }
         return UserValue.LoginRes(username = loginReq.username, token = UUID.randomUUID().toString())
     }
 }
