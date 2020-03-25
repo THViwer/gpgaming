416c8447cf128a0205292cd385553cd01087592e
cabbage <cabbage.cgh@gmail.com>
2020-03-25 16:25:08 +0800

    添加游戏列表

--- gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/PlayerApplication.kt
+++ gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/PlayerApplication.kt
@@ -1,22 +1,24 @@
 package com.onepiece.gpgaming.player
 
+import org.apache.tomcat.util.http.LegacyCookieProcessor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.scheduling.annotation.EnableAsync
-import java.time.LocalDate
-import java.time.LocalDateTime
 import java.util.*
+import org.springframework.boot.web.server.WebServerFactoryCustomizer as WebServerFactoryCustomizer1
 
 
 @SpringBootApplication
 @EnableAsync
 @ComponentScan("com.onepiece.gpgaming")
-open class PlayerApplication {
+open class PlayerApplication: SpringBootServletInitializer() {
 
     @Autowired
     private lateinit var gracefulShutdownTomcat: GracefulShutdownTomcat
@@ -27,6 +29,16 @@ open class PlayerApplication {
         tomcat.addConnectorCustomizers(gracefulShutdownTomcat)
         return tomcat
     }
+
+    @Bean
+    open fun cookieProcessorCustomizer(): WebServerFactoryCustomizer1<TomcatServletWebServerFactory> {
+        return WebServerFactoryCustomizer1{ factory ->
+            factory.addContextCustomizers(TomcatContextCustomizer { content ->
+                content.cookieProcessor = LegacyCookieProcessor()
+            })
+        }
+    }
+
 }
 
 fun main() {
