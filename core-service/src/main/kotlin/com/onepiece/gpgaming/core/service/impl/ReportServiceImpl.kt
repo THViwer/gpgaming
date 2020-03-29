102e05a1f0e1e11742d30631a57c25e5d1d8c895
cabbage <cabbage.cgh@gmail.com>
2020-03-29 18:44:21 +0800

    添加游戏列表

--- core-service/src/main/kotlin/com/onepiece/gpgaming/core/service/impl/ReportServiceImpl.kt
+++ core-service/src/main/kotlin/com/onepiece/gpgaming/core/service/impl/ReportServiceImpl.kt
@@ -113,7 +113,7 @@ class ReportServiceImpl(
                 transferOutReport != null -> transferOutReport.clientId
                 depositReport != null -> depositReport.clientId
                 withdrawReport != null -> withdrawReport.clientId
-                !betMap.isNullOrEmpty() -> betMap[it]!!.first().clientId
+                !betMap.isNullOrEmpty() -> (betMap[it] ?: error("id = ${it}, betMap = $betMap")).first().clientId
                 else -> error(OnePieceExceptionCode.DATA_FAIL)
             }
             MemberDailyReport(id = -1, day = startDate, clientId = clientId, memberId = it, transferIn = transferInReport?.money ?: BigDecimal.ZERO,
