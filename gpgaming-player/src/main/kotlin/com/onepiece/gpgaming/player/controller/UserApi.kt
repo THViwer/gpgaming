440a8cb7f5ffcb2a88996fa743ab30d1737d38c4
cabbage <cabbage.cgh@gmail.com>
2020-05-09 11:31:34 +0800

    init

--- gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/controller/UserApi.kt
+++ gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/controller/UserApi.kt
@@ -1,6 +1,7 @@
 package com.onepiece.gpgaming.player.controller
 
 import com.onepiece.gpgaming.beans.enums.Country
+import com.onepiece.gpgaming.beans.enums.LaunchMethod
 import com.onepiece.gpgaming.player.controller.value.ChangePwdReq
 import com.onepiece.gpgaming.player.controller.value.CheckUsernameResp
 import com.onepiece.gpgaming.player.controller.value.LoginReq
@@ -9,11 +10,11 @@ import com.onepiece.gpgaming.player.controller.value.PlatformMemberUo
 import com.onepiece.gpgaming.player.controller.value.PlatformMemberVo
 import com.onepiece.gpgaming.player.controller.value.RegisterReq
 import io.swagger.annotations.Api
-import io.swagger.annotations.ApiModelProperty
 import io.swagger.annotations.ApiOperation
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 
@@ -21,14 +22,24 @@ import org.springframework.web.bind.annotation.ResponseStatus
 interface UserApi {
 
     @ApiOperation(tags = ["user"], value = "登陆")
-    fun login(@RequestBody loginReq: LoginReq): LoginResp
+    fun login(
+            @RequestHeader("launch", defaultValue = "Web") launch: LaunchMethod,
+            @RequestBody loginReq: LoginReq): LoginResp
+
+    @ApiOperation(tags = ["user"], value = "查看登陆信息")
+    fun loginDetail(
+            @RequestHeader("launch", defaultValue = "Web") launch: LaunchMethod
+    ): LoginResp
 
     @ApiOperation(tags = ["user"], value = "更新自动转账配置")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun upAutoTransfer(@RequestParam("autoTransfer") autoTransfer: Boolean)
 
     @ApiOperation(tags = ["user"], value = "注册")
-    fun register(@RequestBody registerReq: RegisterReq): LoginResp
+    fun register(
+            @RequestHeader("launch", defaultValue = "Web") launch: LaunchMethod,
+            @RequestBody registerReq: RegisterReq
+    ): LoginResp
 
     @ApiOperation(tags = ["user"], value = "可选国家列表")
     fun countries(): List<Country>
--- gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/controller/UserApiController.kt
+++ gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/controller/UserApiController.kt
@@ -1,6 +1,7 @@
 package com.onepiece.gpgaming.player.controller
 
 import com.onepiece.gpgaming.beans.enums.Country
+import com.onepiece.gpgaming.beans.enums.LaunchMethod
 import com.onepiece.gpgaming.beans.enums.Platform
 import com.onepiece.gpgaming.beans.enums.Role
 import com.onepiece.gpgaming.beans.model.token.PlaytechClientToken
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -39,8 +41,11 @@ class UserApiController(
 ) : BasicController(), UserApi {
 
     @PostMapping
-    override fun login(@RequestBody loginReq: LoginReq): LoginResp {
-        //TODO 判断域名来选择厅主
+    override fun login(
+            @RequestHeader("launch", defaultValue = "Web") launch: LaunchMethod,
+            @RequestBody loginReq: LoginReq
+    ): LoginResp {
+
         val bossId = getBossIdByDomain()
 
         val loginValue = LoginValue(bossId = bossId, username = loginReq.username, password = loginReq.password, ip = getIpAddress())
@@ -49,9 +54,34 @@ class UserApiController(
 
         val webSite = webSiteService.all().first { it.clientId == member.clientId }
 
+        val isMobile = if (launch == LaunchMethod.Wap) "/m" else ""
         val token = authService.login(clientId = member.clientId, username = loginReq.username)
         return LoginResp(id = member.id, role = Role.Member, username = member.username, token = token, name = member.name, autoTransfer = member.autoTransfer,
-                domain = webSite.domain)
+                domain = "https://www.${webSite.domain}${isMobile}")
+    }
+
+
+    @GetMapping("/login/detail")
+    override fun loginDetail(
+            @RequestHeader("launch", defaultValue = "Web") launch: LaunchMethod
+    ): LoginResp {
+
+        val user = this.currentUser()
+
+        val member = memberService.getMember(user.id)
+
+
+        val authHeader = this.getRequest().getHeader("Authorization")
+        val authToken = authHeader.substring("Bearer ".length) // The part after "Bearer "
+
+
+        val webSite = webSiteService.all().first { it.clientId == member.clientId }
+
+
+        val isMobile = if (launch == LaunchMethod.Wap) "/m" else ""
+        return LoginResp(id = member.id, role = Role.Member, username = member.username, token = authToken, name = member.name, autoTransfer = member.autoTransfer,
+                domain = "https://www.${webSite.domain}${isMobile}")
+
     }
 
     @PutMapping("/config")
@@ -61,7 +91,10 @@ class UserApiController(
     }
 
     @PutMapping
-    override fun register(@RequestBody registerReq: RegisterReq): LoginResp {
+    override fun register(
+            @RequestHeader("launch", defaultValue = "Web") launch: LaunchMethod,
+            @RequestBody registerReq: RegisterReq
+    ): LoginResp {
 
         check(registerReq.country != Country.Default)
 
@@ -75,7 +108,7 @@ class UserApiController(
         memberService.create(memberCo)
 
         val loginReq = LoginReq(username = registerReq.username, password = registerReq.password)
-        return this.login(loginReq)
+        return this.login(launch, loginReq)
     }
 
     @GetMapping("/country")
