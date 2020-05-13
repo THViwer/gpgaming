82baf315a80d1bd32f6eccb4cbbbbbc07012412a
cabbage <cabbage.cgh@gmail.com>
2020-05-13 16:56:33 +0800

    init

--- core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/enums/PromotionCategory.kt
+++ core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/enums/PromotionCategory.kt
@@ -5,18 +5,27 @@ enum class PromotionCategory(
         val cname: String
 ) {
 
+    // 首充
     First("New Member", "首充"),
 
+    // 特别优惠
     Special("special promotion", "特别优惠"),
 
+    // 返水优惠
+    Backwater("backwater", "返水"),
+
+    // 其它
+    Other("other", "其它"),
+
+
+    // 用作显示的优惠
     Slot("slot", "老虎机"),
 
     Live("live casino", "真人视频"),
 
     Sport("sport", "体育"),
 
-    Fishing("fishing", "捕鱼"),
+    Fishing("fishing", "捕鱼")
 
-    Backwater("backwater", "返水")
 
 }
