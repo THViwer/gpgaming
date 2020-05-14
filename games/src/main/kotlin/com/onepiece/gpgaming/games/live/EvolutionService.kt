307c8f6ed7cbc510734634df0825047c8b2a13d2
cabbage <cabbage.cgh@gmail.com>
2020-05-14 16:44:30 +0800

    init

--- games/src/main/kotlin/com/onepiece/gpgaming/games/live/EvolutionService.kt
+++ games/src/main/kotlin/com/onepiece/gpgaming/games/live/EvolutionService.kt
@@ -118,43 +118,6 @@ class EvolutionService : PlatformService() {
             else -> "en"
         }
 
-//        val json = """
-//            {
-//               "uuid":"$uuid",
-//               "player":{
-//                  "id":"${startReq.username}",
-//                  "update":true,
-//                  "firstName":"firstName",
-//                  "lastName":"lastName",
-//                  "nickname":"nickname",
-//                  "country":"MY",
-//                  "language":"$lang",
-//                  "currency":"${token.currency}",
-//                  "session":{
-//                     "id":"$uuid",
-//                     "ip":"$ip"
-//                  }
-//               },
-//               "config":{
-//                  "brand":{
-//                     "id":"1",
-//                     "skin":"1"
-//                  },
-//                  "channel":{
-//                     "wrapped":false,
-//                     "mobile":false
-//                  },
-//                  "urls":{
-//                     "cashier":"http://www.chs.ee",
-//                     "responsibleGaming":"http://www.RGam.ee",
-//                     "lobby":"http://www.lobb.ee",
-//                     "sessionTimeout":"http://www.sesstm.ee"
-//                  }
-//               }
-//            }
-//
-//        """.trimIndent()
-
         val json = """
             {
                "uuid":"$uuid",
