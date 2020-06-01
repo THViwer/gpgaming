052afba2421e128b5e37eb91d7b7614d9e39f3db
cabbage <cabbage.cgh@gmail.com>
2020-06-01 20:42:13 +0800

    init

--- payment/src/main/kotlin/com/onepiece/gpgaming/payment/GPPayService.kt
+++ payment/src/main/kotlin/com/onepiece/gpgaming/payment/GPPayService.kt
@@ -1,9 +1,11 @@
 package com.onepiece.gpgaming.payment
 
+import com.onepiece.gpgaming.beans.enums.Bank
 import com.onepiece.gpgaming.beans.model.pay.GPPayConfig
 import com.onepiece.gpgaming.payment.http.PayOkHttpUtil
 import org.apache.commons.codec.digest.DigestUtils
 import org.springframework.stereotype.Service
+import java.math.BigDecimal
 
 
 data class GPPayResponse(
@@ -18,6 +20,24 @@ data class GPPayResponse(
         val payUrl: String
 )
 
+data class GPPayReq(
+
+        // 商户Code
+        val merchantCode: String,
+
+        // 商户端订单Id
+        val orderId: String,
+
+        // 金额
+        val amount: BigDecimal,
+
+        // 银行
+        val bank: Bank,
+
+        // 商户回调地址
+        val merchantBackPath: String = "http://localhost:8011/api/v1/admin/demo/pay"
+)
+
 @Service
 class GPPayService(
         private val okHttpUtil: PayOkHttpUtil
@@ -30,15 +50,18 @@ class GPPayService(
         val signParam = ""
         val token =  DigestUtils.md5Hex(signParam)
 
-        val param = mapOf<String, Any>(
-                "merchantCode" to config.merchantId,
-                "orderId" to req.orderId,
-                "amount" to req.amount.setScale(2, 2),
-                "bank" to req.selectBank!!,
-                "merchantBackPath" to config.backendURL
-        ).toMap()
+//        val param = mapOf<String, Any>(
+//                "merchantCode" to config.merchantId,
+//                "orderId" to req.orderId,
+//                "amount" to req.amount.setScale(2, 2),
+//                "bank" to req.selectBank!!,
+//                "merchantBackPath" to config.backendURL
+//        ).toMap()
 
-        val response = okHttpUtil.doPostJson(url = config.apiPath, data = param, clz = GPPayResponse::class.java)
+        val req = GPPayReq(merchantCode = config.merchantId, orderId = req.orderId, amount = req.amount.setScale(2, 2),
+                bank = req.selectBank!!, merchantBackPath = config.backendURL)
+
+        val response = okHttpUtil.doPostJson(url = config.apiPath, data = req, clz = GPPayResponse::class.java)
 
 
         return mapOf(
