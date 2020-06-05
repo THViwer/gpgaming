d3a9da22004e01eb1bfc09dd891b2089caede8be
cabbage <cabbage.cgh@gmail.com>
2020-06-05 14:30:41 +0800

    init

---
+++ core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/model/AgentConfig.kt
@@ -0,0 +1,23 @@
+//package com.onepiece.gpgaming.beans.model
+//
+//import java.time.LocalDateTime
+//
+///**
+// * 代理配置
+// */
+//data class AgentConfig(
+//
+//        // id
+//        val id: Int,
+//
+//        val whatsApps: String,
+//
+//        val emails: String,
+//
+//
+//
+//
+//        // 创建时间
+//        val createdTime: LocalDateTime
+//
+//)
