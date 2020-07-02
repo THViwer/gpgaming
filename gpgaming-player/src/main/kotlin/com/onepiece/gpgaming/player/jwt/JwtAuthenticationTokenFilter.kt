8159742f22e998ae71390c55fe88ec74506f8c8a
cabbage <cabbage.cgh@gmail.com>
2020-07-02 19:45:07 +0800

    init

--- gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/jwt/JwtAuthenticationTokenFilter.kt
+++ gpgaming-player/src/main/kotlin/com/onepiece/gpgaming/player/jwt/JwtAuthenticationTokenFilter.kt
@@ -45,7 +45,7 @@ open class JwtAuthenticationTokenFilter(
             if (SecurityContextHolder.getContext().authentication == null) {
 
                 val userDetails = tokenStore.readAccessToken(authToken)
-                username = userDetails?.musername
+                username = userDetails?.musername?.split("@")?.last()
 
                 if (userDetails != null && jwtTokenUtil.validateToken(authToken, userDetails)) {
                     val authentication = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
