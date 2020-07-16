c603e3056d172152491b32692bf5333ae28465e1
cabbage <cabbage.cgh@gmail.com>
2020-07-16 16:02:10 +0800

    init

--- gpgaming-admin/src/main/kotlin/com/onepiece/gpgaming/web/controller/DemoController.kt
+++ gpgaming-admin/src/main/kotlin/com/onepiece/gpgaming/web/controller/DemoController.kt
@@ -8,6 +8,7 @@ import com.onepiece.gpgaming.core.service.MemberService
 import com.onepiece.gpgaming.core.service.PlatformBindService
 import com.onepiece.gpgaming.core.service.WaiterService
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -43,6 +44,7 @@ class DemoController(
 //    }
 
 
+    @GetMapping("/info/init")
     fun initMemberInfo() {
 
         val memberQuery = MemberQuery()
