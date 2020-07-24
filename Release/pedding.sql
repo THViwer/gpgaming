931d18b1b82b43152f1fcd192455c7aa4e219752
cabbage <cabbage.cgh@gmail.com>
2020-07-24 11:53:37 +0800

    init

--- Release/pedding.sql
+++ Release/pedding.sql
@@ -1,4 +1,4 @@
 ALTER TABLE sale_daily_report ADD own_member_count INT DEFAULT 0;
-ALTER TABLE sale_daily_report ADD own_system_count INT DEFAULT 0;
+ALTER TABLE sale_daily_report ADD system_member_count INT DEFAULT 0;
 ALTER TABLE sale_month_report ADD system_member_count INT DEFAULT 0;
 ALTER TABLE sale_month_report ADD own_system_count INT DEFAULT 0;
