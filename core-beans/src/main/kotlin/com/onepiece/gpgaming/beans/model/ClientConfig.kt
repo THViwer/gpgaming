e5a07ed724e12d391dfdceda0d140511b88768f5
cabbage <cabbage.cgh@gmail.com>
2020-08-17 16:37:16 +0800

    init

--- core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/model/ClientConfig.kt
+++ core-beans/src/main/kotlin/com/onepiece/gpgaming/beans/model/ClientConfig.kt
@@ -1,56 +1,58 @@
 package com.onepiece.gpgaming.beans.model
 
 import com.onepiece.gpgaming.beans.enums.ShowPosition
+import java.math.BigDecimal
 import java.time.LocalDateTime
 
 data class ClientConfig(
 
         // id
         val id: Int,
-
         // 厅主
         val clientId: Int,
 
+        /** seo相关设置 */
         // 标题
         val title: String,
-
         // 关键字
         val keywords: String,
-
         // 描述
         val description: String,
-
-        // 在线聊天Id
-        val liveChatId: String,
-
-        // 是否打开新的页面
-        val liveChatTab: Boolean,
-
         // google统计Id
         val googleStatisticsId: String,
-
         //面子书广告
         val facebookTr: String,
-
         // facebook显示位置
         val facebookShowPosition: ShowPosition,
-
         // asg 广告内容
         val asgContent: String,
 
+        /** live chat  */
+        // 在线聊天Id
+        val liveChatId: String,
+        // 是否打开新的页面
+        val liveChatTab: Boolean,
 
+        /** 短信 */
         // 是否开启注册消息
         val enableRegisterMessage: Boolean,
-
         // 短信注册模板
-        val registerMessageTemplate:  String,
+        val registerMessageTemplate: String,
 
 
+        /** 会员推广佣金 */
         // 是否开启会员介绍
-        val enableIntroduce:  Boolean,
-
+        val enableIntroduce: Boolean,
         // 会员介绍优惠活动Id(category=Introduce)
         val introducePromotionId: Int,
+        // 注册佣金
+        val registerCommission: BigDecimal,
+        // 需要充值金额
+        val needDeposit: BigDecimal,
+        // 充值周期
+        val depositPeriod: BigDecimal,
+        // 充值佣金
+        val depositCommission: BigDecimal,
 
 
         // 创建时间
