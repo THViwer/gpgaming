af2dfebbff2fc8f88edfa6e9a3d3440cde64d750
cabbage <cabbage.cgh@gmail.com>
2020-12-18 17:21:16 +0800

    `init`

--- gpgaming-admin/src/main/kotlin/com/onepiece/gpgaming/web/controller/IndexApi.kt
+++ gpgaming-admin/src/main/kotlin/com/onepiece/gpgaming/web/controller/IndexApi.kt
@@ -44,7 +44,7 @@ interface IndexApi {
             @RequestParam("description") description: String,
             @RequestParam("liveChatId") liveChatId: String,
             @RequestParam("liveChatTab") liveChatTab: Boolean,
-            @RequestParam("gtag") gtag: String,
+            @RequestParam("gtag", defaultValue = "") gtag: String,
             @RequestParam("googleStatisticsId") googleStatisticsId: String,
             @RequestParam("facebookTr") facebookTr: String,
             @RequestParam("facebookShowPosition") facebookShowPosition: ShowPosition,
