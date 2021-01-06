66596556ff1baab84c1d7758d5cecac42a98bd5b
cabbage <cabbage.cgh@gmail.com>
2021-01-06 13:31:06 +0800

    s

--- games/src/main/kotlin/com/onepiece/gpgaming/games/live/EBetService.kt
+++ games/src/main/kotlin/com/onepiece/gpgaming/games/live/EBetService.kt
@@ -110,7 +110,7 @@ class EBetService : PlatformService() {
                 "rechargeReqId" to checkTransferReq.orderId,
                 "currency" to clientToken.currency,
                 // 为了签名
-                "username" to checkTransferReq.username
+                "username" to checkTransferReq.orderId
         )
 
         val okResponse = this.doPost(data = data, clientToken = clientToken, path = "/api/rechargestatus")
